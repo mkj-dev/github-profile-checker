47e8bfdd420f4fa74e8d5168bcff422856d5b088
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-02-19 15:17:22 +0100

    Fetch github profile data and add <profile-data> component to the template

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,10 +1,16 @@
 const searchBar = {
-    template:
-    /* html*/
-    `<div>
-        <input type="text" id="search-bar" placeholder="Github profile name">
-        <button type="button" id="search-button" @click="getProfileData">Search</button>
-    </div>`,
+    data() {
+        return {
+            login: '',
+            name: '',
+            avatar_url: '',
+            html_url: '',
+            location: '',
+            public_repos: 0,
+            followers: 0,
+            following: 0
+        }
+    },
     methods: {
         getProfileData() {
             const searchBar = document.getElementById('search-bar');
@@ -12,8 +18,33 @@ const searchBar = {
             fetch(`https://api.github.com/users/${username}`)
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data);
+                    this.login = data['login'];
+                    this.name = data['name'];
+                    this.avatar_url = data['avatar_url'];
+                    this.html_url = data['html_url'];
+                    this.location = data['location'];
+                    this.public_repos = data['public_repos'];
+                    this.followers = data['followers'];
+                    this.following = data['following'];
                 });
+            
         }
-    }
+    },
+    template:
+    /* html*/
+    `<div>
+        <input type="text" id="search-bar" placeholder="Github profile name">
+        <button type="button" id="search-button" @click="getProfileData">Search</button>
+        <profile-data
+            :login="login"
+            :name="name"
+            :avatar_url="avatar_url"
+            :html_url="html_url"
+            :location="location"
+            :public_repos="public_repos"
+            :followers="followers"
+            :following="following"
+        >
+        </profile-data>
+    </div>`
 };
